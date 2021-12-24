44abffa0972994e9a5cae6da8f7122548e374e03
Raziur306 <raziur.rahaman.ronju@gmail.com>
2021-12-25 02:31:17 +0600

    Splash Screen

---
+++ app/src/main/java/com/chatbiz/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.chatbiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+
+    }
+}
