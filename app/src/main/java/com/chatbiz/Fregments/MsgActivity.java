0c13defce465124b52a0ca48a8641c68511f63c0
Raziur306 <raziur.rahaman.ronju@gmail.com>
2021-12-25 12:22:12 +0600

    Added Activity

---
+++ app/src/main/java/com/chatbiz/Fregments/MsgActivity.java
@@ -0,0 +1,25 @@
+package com.chatbiz.Fregments;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chatbiz.R;
+
+public class MsgActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.activity_msg, container,false);
+
+
+        return view;
+    }
+}
