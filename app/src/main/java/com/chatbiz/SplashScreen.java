a1a2c622ca2ff45abf8023dda742efda8f1315cc
Raziur306 <raziur.rahaman.ronju@gmail.com>
2021-12-25 19:11:01 +0600

    Firebase Connection

--- app/src/main/java/com/chatbiz/SplashScreen.java
+++ app/src/main/java/com/chatbiz/SplashScreen.java
@@ -17,7 +17,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                startActivity(new Intent(SplashScreen.this,RegistrationActivity.class));
                 finish();
             }
         },3000);
